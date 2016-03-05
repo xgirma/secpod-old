3ce301595c21c5dea1c6809001f3888060c92400
Girma Nigusse <xgirma@gmail.com>
2016-03-05 16:45:21 -0500

    update route

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,28 @@
 exports.index =  function(request, response){
     response.render('index', {
-        title: this.apptitle
+        title: this.apptitle,
+        classname: 'home'
     });
 };
 
 exports.list = function(request, response){
-    response.render('list', {
-        title: 'Podcast List'
+    response.render('index', {
+        title: 'Podcast List',
+        classname: 'list'
     });
 };
 
 exports.details = function(request, response){
     var name = request.params.name;
-    response.render('detail', {
-        title: name + "'s Podcast Detail"
+    response.render('index', {
+        title: name + "'s Podcast Detail",
+        classname: 'detail'
     });
 };
 
 exports.badroute = function(request, response){
-    response.render('badroute',{
-        title: 'You are lost create a podcast :) ...'
+    response.render('index',{
+        title: 'You are lost ...',
+        classname: 'badroute'
     });
 };
