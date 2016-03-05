ca2940e1fd439b43700114007f20f2d06ce53e8f
Girma Nigusse <xgirma@gmail.com>
2016-03-05 14:03:29 -0500

    express init

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ app.get('/', function(request, response){
     response.send('<h1> Security Podcast directory</h1>');
 });
 
+app.get('/list', function(request, response){
+    response.send('<h1> Security Podcast directory</h1>');
+});
+
+app.get('/list/:name', function(request, response){
+    var name = request.params.name;
+    response.send('<h1>' + name + ' podcast ...</h1>');
+});
+
+app.get('*', function(request, response){
+    response.send('<h1> bad route ... </h1>');
+});
+
 var server = app.listen('3000', function(){
     console.log('Server listening at port 30000');
 });
