e02204170dccbfd2b14aa141d316381f1ede23a3
Girma Nigusse <xgirma@gmail.com>
2016-03-05 13:48:58 -0500

    express init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # secpod
 A security podcast directory built using express.js and node.js
+
+    > npm install
+    > nodemon app.js
+    > localhost:3000
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var http = require('http');
-var myServer = http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h1>Security Podcast directory</h1>');
-    response.end();
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response){
+    response.send('<h1> Security Podcast directory</h1>');
 });
 
-myServer.listen(3000, function(){
-    console.log('Server listening at port 3000')
+var server = app.listen('3000', function(){
+    console.log('Server listening at port 30000');
 });
