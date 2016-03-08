ccf65c57fbc3968c958bcf9d1ab73b6dcc8995ac
Girma Nigusse <xgirma@gmail.com>
2016-03-07 19:51:00 -0500

    add 404 page

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ if (app.get('env') === 'development') {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
-    message: err.message,
+    message: {},
     error: {}
   });
 });
