3c2213a9650356a9c3af54c71cafb216668d41be
Girma Nigusse <xgirma@gmail.com>
2016-03-19 21:16:55 -0400

    update router for list page

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,10 @@ var router = express.Router();
 var Podcast = require('../models/podcast');
 
 router.get('/', function(req, res, next) {
-  Podcast.all(function(err, docs){
-    if(err){
-      console.log('Error finding doc ...');
-    }
-
     Podcast.rated(function(err, top){
       if(err){
         console.log('Error finding docs with 5 star')
       }
-
-      console.log('top', top);
 
       Podcast.featured(function(err, doc){
         if(err){
@@ -23,20 +16,24 @@ router.get('/', function(req, res, next) {
         res.render('index', {
           page: 'index',
           title: 'Security Podcast Directory',
-          podcast: docs,
           featured: doc,
           selected: top
         });
       });
 
     });
-  });
 });
 
 router.get('/list', function(req, res, next) {
-  res.render('list', {
-    page: 'list',
-    title: 'Directory'
+  Podcast.all(function(err, docs) {
+    if (err) {
+      console.log('Error finding doc ...');
+    }
+    res.render('list', {
+      page: 'list',
+      title: 'Directory',
+      podcast: docs
+    });
   });
 });
 
