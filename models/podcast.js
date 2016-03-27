5e003bb9f0fb0d75ac0b3b81404f8756be6d797f
Girma Nigusse <xgirma@gmail.com>
2016-03-27 16:13:50 -0400

    change pub_date to date

--- models/podcast.js
+++ models/podcast.js
@@ -16,7 +16,7 @@ exports.featured = function(cb){
 
   collection.find()
     .limit(1)
-    .skip(Math.floor(Math.random() * 9))
+    .skip(Math.floor(Math.random() * 18))
     .toArray(function(err, docs){
       cb(err, docs);
   });
