32f1827ce76be7ebcc95a7b3903e9e74bce79831
Girma Nigusse <xgirma@gmail.com>
2016-04-04 00:22:43 -0400

    add like count

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -17,14 +17,16 @@ function stopHomeAudio() {
 /* list page */
 var listAudio = document.getElementById('selected-audio');
 var listTitle = document.getElementById('selected-title');
+var listLike = document.getElementById('x');
 var nowPlaying = document.getElementById('now-playing');
 
-function playListAudioFromList(audio, title) {
+function playListAudioFromList(audio, title, like) {
   nowPlaying.style.display = "block";
   if(getCurrentListAudio() !== audio){
     listAudio.setAttribute('src', audio);
     listAudio.load();
     listTitle.innerHTML = title;
+    listLike.innerHTML = like;
     listAudio.play();
   }
 
