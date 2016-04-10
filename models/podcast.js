c896d88b1728da458b7a14fb93da5a4a7c775ed3
Girma Nigusse <xgirma@gmail.com>
2016-04-10 16:10:17 -0400

    add method to update add source to featured

--- models/podcast.js
+++ models/podcast.js
@@ -4,6 +4,7 @@ var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 var assert = require('assert');
 var url = 'mongodb://secpood:53cpo0d@ds025429.mlab.com:25429/secpood';
+var Encoder = require('node-html-encoder').Encoder;
 
 exports.all = function (cb) {
   MongoClient.connect(url, function (err, db) {
@@ -53,20 +54,13 @@ exports.topTen = function (cb) {
   });
 };
 
-// TODO remove
-exports.rated = function (cb) {
+exports.updatedb = function (feed, cb) {
   MongoClient.connect(url, function (err, db) {
     assert.equal(null, err);
 
-    db.collection('podcast')
-      .find({rating: '5'})
-      .limit(10)
-      .sort({'author': 1, 'episode': 1})
-      .toArray(function (err, docs) {
-        assert.equal(null, err);
-        db.close();
-        cb(err, docs);
-      });
+    insertDocument(feed, db, function() {
+      // db.close();
+    });
   });
 };
 
@@ -99,5 +93,55 @@ exports.etc = function (cb) {
         db.close();
         cb(err, docs);
       });
+  });
+};
+
+var insertDocument = function (feed, db, callback) {
+  var encoder = new Encoder('entity');
+  var author = 'no author';
+  var title = 'not title';
+  var pubDate = 'no publication date';
+  var rss = 'no rss';
+  var link = 'no link';
+  var audio = 'no audio';
+  var intro = 'no intro';
+  var like = 0;
+
+
+  request(rssMe, function (error, resp, body) {
+    parser(body, function (error, ret) {
+      for (var i = 0; i <= (ret.items.length - 1); i++) {
+
+        author = ret.items[i].author;
+        title = ret.items[i].title;
+        pubDate = ret.items[i].date;
+        rss = feed;
+        link = ret.items[i].link;
+        audio = ret.enclosure[i];
+        intro = encoder.htmlEncode(ret.items[i].description) ;
+        like = 0;
+
+        db.collection('podcast').update({
+            "audio": audio
+          }, {
+            "author": author,
+            "title": title,
+            "date": pubDate,
+            "rss": rss,
+            "link": link,
+            "audio": audio,
+            "intro": intro,
+            "like": like
+          },
+          {
+            upsert: true
+          },
+
+          function (err, result) {
+            assert.equal(err, null);
+            callback();
+          });
+      }
+    });
   });
 };
